08cd642ec67e84e00d6399db440866c7f8db3279
kotknyaz <kotKnyaz@mail.ru>
2024-04-14 09:41:06 +0300

    Простейший вывод в 2D с видом сверху

--- ptdGame/ptdGame.cpp
+++ ptdGame/ptdGame.cpp
@@ -1,10 +1,163 @@
 #include "ptdGame.h"
-#include <iostream>
 
 namespace ptd {
-    int func() {
-        std::cout << "Hello world!" << std::endl;
+    const double FOV_DIVIDE_BY2 = (M_PI / 3) / 2;
+
+    MainMenu::MainMenu()
+    {
+        window = new sf::RenderWindow(sf::VideoMode(800, 600), "My window");
+
+        Menu();
+    }
+
+    int MainMenu::Menu()
+    {
+        while (window->isOpen())
+        {
+            sf::Event event;
+            while (window->pollEvent(event))
+            {
+                switch (event.type)
+                {
+                case sf::Event::KeyPressed:     //начало игры
+                {
+                    GameManager gm;
+                    Interpreter inter(window, &gm);
+                    break;
+                }
+                case sf::Event::Closed:         //закрытие окна
+                {
+                    window->close();
+                    break;
+                }
+                }
+            }
+        }
 
         return 0;
+    }
+
+
+    std::vector<Coord> GameManager::GetVisibleWalls()
+    {
+
+    }
+
+    GameManager::GameManager()
+    {
+        walls.push_back(new Coord(-2, 4));
+        walls.push_back(new Coord(3, 4));
+        walls.push_back(new Coord(3, 4));
+        walls.push_back(new Coord(3, -4));
+        walls.push_back(new Coord(3, -4));
+        walls.push_back(new Coord(-2, -4));
+        walls.push_back(new Coord(-2, -4));
+        walls.push_back(new Coord(-2, 4));
+    }
+
+    PrintInfo GameManager::Update(UpdateInfo& updateInfo)
+    {
+        PrintInfo printInfo;
+
+        // temp
+        printInfo.walls = walls;
+        printInfo.playerPos.x = playerPos.x;
+        printInfo.playerPos.y = playerPos.y;
+        printInfo.viewLeft = view + FOV_DIVIDE_BY2;
+        printInfo.viewRight = view - FOV_DIVIDE_BY2;
+
+
+
+        return printInfo;
+    }
+
+
+
+    Interpreter::Interpreter(sf::RenderWindow* activeWindow, GameManager* gameManager) : window(activeWindow), gm(gameManager)
+    {
+        GameProcess();
+    }
+
+    int Interpreter::GameProcess()
+    {
+        
+        while (window->isOpen())
+        {
+            sf::Event event;
+            while (window->pollEvent(event))
+            {
+                switch (event.type)
+                {
+                case sf::Event::Closed:         //закрытие окна
+                {
+                    window->close();
+                    break;
+                }
+                }
+            }
+            UpdateInfo toSent;
+            toSent.view = 0;
+            Print( gm->Update(toSent) );
+        }
+    }
+
+    int Interpreter::Print(PrintInfo& info)
+    {
+        window->clear(sf::Color::Black);
+        //вид сверху; центр - игрок; все стены х koef; y - вверх, х - вправо
+        // стены - синий; взгляд - зеленый; видимые стены - красный; игрок - белый
+        double koef = 40;
+        double rayLenth = 500;
+        sf::VertexArray vertexWalls(sf::PrimitiveType::Lines, 0);
+        double halfScreenX = window->getSize().x / 2;
+        double halfScreenY = window->getSize().y / 2;
+
+        for (int i = 0; i < info.walls.size(); ++i)
+        {
+            vertexWalls.append(sf::Vertex(sf::Vector2f(halfScreenX - (info.walls[i]->x + info.playerPos.x) * koef,
+                halfScreenY - (info.walls[i]->y - info.playerPos.y) * koef), sf::Color::Blue));
+        }
+        
+        sf::VertexArray vertexPlayer(sf::PrimitiveType::Lines);
+        vertexPlayer.append(sf::Vertex(sf::Vector2f(halfScreenX - 5, halfScreenY - 5), sf::Color::White));
+        vertexPlayer.append(sf::Vertex(sf::Vector2f(halfScreenX + 5, halfScreenY + 5), sf::Color::White));
+        vertexPlayer.append(sf::Vertex(sf::Vector2f(halfScreenX + 5, halfScreenY - 5), sf::Color::White));
+        vertexPlayer.append(sf::Vertex(sf::Vector2f(halfScreenX - 5, halfScreenY + 5), sf::Color::White));
+
+        sf::VertexArray vertexRays(sf::PrimitiveType::Lines, 0);
+        vertexRays.append(sf::Vertex(sf::Vector2f(halfScreenX, halfScreenY), sf::Color::Green));
+        vertexRays.append(sf::Vertex(sf::Vector2f(halfScreenX + cos(info.viewLeft) * rayLenth,
+            halfScreenY - sin(info.viewLeft) * rayLenth), sf::Color::Green));
+        vertexRays.append(sf::Vertex(sf::Vector2f(halfScreenX, halfScreenY), sf::Color::Green));
+        vertexRays.append(sf::Vertex(sf::Vector2f(halfScreenX + cos(info.viewRight) * rayLenth,
+            halfScreenY - sin(info.viewRight) * rayLenth), sf::Color::Green));
+
+        sf::VertexArray vertexVisibleWalls(sf::PrimitiveType::Lines, 0);
+        for (int i = 0; i < info.visibleWalls.size(); ++i)
+        {
+            vertexWalls.append(sf::Vertex(sf::Vector2f(halfScreenX - (info.visibleWalls[i].x + info.playerPos.x) * koef,
+                halfScreenY - (info.visibleWalls[i].y - info.playerPos.y) * koef), sf::Color::Red));
+        }
+
+
+
+        window->draw(vertexPlayer);
+        window->draw(vertexWalls);
+        window->draw(vertexRays);
+        window->draw(vertexVisibleWalls);
+        window->display();
+        return 0;
+    }
+
+
+
+    Coord::Coord(double newX, double newY) : x(newX), y(newY) {}
+
+    Coord::Coord() {}
+
+
+    PrintInfo::PrintInfo()
+    {
+        playerPos = Coord(0.f, 0.f);
     }
 }
--- ptdGame/ptdGame.h
+++ ptdGame/ptdGame.h
@@ -1,8 +1,76 @@
 #ifndef PTDGAME_H
 #define PTDGAME_H
 
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+
 namespace ptd {
-    int func();
+
+
+
+	struct Coord
+	{
+		double x = 0;
+		double y = 0;
+		Coord(double newX, double newY);
+		Coord();
+	};
+	struct UpdateInfo
+	{
+		double view; // temp напрвление центра взгляда
+	};
+	struct PrintInfo
+	{
+		//temp
+		Coord playerPos;
+		std::vector<Coord*> walls;
+		std::vector<Coord> visibleWalls;
+		double viewLeft;
+		double viewRight;
+
+		PrintInfo();
+	};
+
+
+
+	class MainMenu {
+		sf::RenderWindow *window;
+
+	public:
+		MainMenu();
+		int Menu();
+	};
+
+
+	class GameManager {
+
+		std::vector<Coord*> walls; // temp
+		Coord playerPos;
+		double view;
+
+		std::vector<Coord> GetVisibleWalls();
+
+	public:
+		GameManager(); // пока пусто
+		PrintInfo Update(UpdateInfo&);
+	};
+
+
+	class Interpreter {
+		sf::RenderWindow* window;
+		GameManager* gm;
+	public:
+		Interpreter(sf::RenderWindow*, GameManager*);
+		int GameProcess();
+		int Print(PrintInfo&);
+
+	};
+
 }
 
 #endif //PTDGAME_H
